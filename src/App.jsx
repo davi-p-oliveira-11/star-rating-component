af7d669a92692595e6a8c69efff78830fc10aee0
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-21 14:20:20 -0300

    changed the function type

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { StarRating } from './components/StarRating'
-import './App.css'
 
-function App() {
-   <StarRating />
+const App = () => {
+   <StarRating numberOfStars={6}/>
 }
 
 export default App
