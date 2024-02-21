e0f27b3380594ee56856b3612712d586ba671775
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-02-21 16:24:32 -0300

    added functions and use states

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -2,9 +2,29 @@ import { FaStar } from "react-icons/fa";
 import { useState } from "react";
 import '../index.css'
 
-const StarRating = () => {
+const StarRating = (numberOfStars = 6) => {
+  const [rating, SetRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
+  function handleClick(getCurrentIndex) {
+
+  }
+
+  function handleMouseEnter(getCurrentIndex) {
+    
+  }
+
+  function handleMouseLeave(getCurrentIndex) {
+    
+  }
+
+
   return (
-    <div className="">
+    <div className="star-rating">
+      {[...Array(numberOfStars).map((_, index) => {
+        index += 1;
+
+      })]}
       <div className="wrapper">
        <h3>Rate Us:</h3>
       </div>
